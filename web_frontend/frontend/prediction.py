f72554ec376628b7e3c771013b8b84435ae0f08a
Stefan Caminada <scam87@gmail.com>
2023-01-02 19:24:40 +0100

    type problem

--- web_frontend/frontend/prediction.py
+++ web_frontend/frontend/prediction.py
@@ -41,7 +41,7 @@ def index()->str:
         #https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/
         #response: dict = get('https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/HousePricePrediction', params=params, headers={'Content-Type': 'application/json'}).json()
         #response_json: str = json.dumps(response)
-        response_json = json.dumps('{"predicted_price":790000}')
+        response_json = "{'predicted_price':790000}"
         db = get_db()
         db.execute(f"INSERT INTO predictions (user_ip, query_data, predicted_price) VALUES ('127.0.0.1', '{params_json}', '{response_json}')")
         db.commit()
