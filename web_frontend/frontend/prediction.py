e77c9be42f03b185a7c84c90936af5a81a1d899c
Stefan Caminada <scam87@gmail.com>
2023-01-06 14:59:23 +0100

    interface test

--- web_frontend/frontend/prediction.py
+++ web_frontend/frontend/prediction.py
@@ -41,7 +41,7 @@ def index()->str:
         #https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/
         #response: dict = get('https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/HousePricePrediction', params=params, headers={'Content-Type': 'application/json'}).json()
         print('before get:')
-        response: dict = urlfetch.fetch(
+        response = urlfetch.fetch(
             url='https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/HousePricePrediction',
             params=params,
             method=urlfetch.GET,
@@ -58,9 +58,7 @@ def index()->str:
         db = get_db()
         db.execute(f"INSERT INTO predictions (user_ip, query_data, predicted_price) VALUES ('127.0.0.1', '{params_json}', '{response_json}')")
         db.commit()
-        return redirect(url_for('prediction.recent'))
-    else:
-        return render_template('prediction/index.html')
+    return render_template('prediction/index.html')
 
 @bp.route('/recent')
 def recent()->str:
