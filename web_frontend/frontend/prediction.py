779a643186b8c55637eaebff3908c954d9e94105
Stefan Caminada <scam87@gmail.com>
2023-01-06 11:08:50 +0100

    Removed Errorhandling

--- web_frontend/frontend/prediction.py
+++ web_frontend/frontend/prediction.py
@@ -43,16 +43,13 @@ def index()->str:
         #response: dict = get('http://web:5000/HousePricePrediction', params=params, headers={'Content-Type': 'application/json'}).json()
         #https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/
         #response: dict = get('https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/HousePricePrediction', params=params, headers={'Content-Type': 'application/json'}).json()
-        try: 
-            response: dict = urlfetch.fetch(
-                url='https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/HousePricePrediction',
-                params=params,
-                method=urlfetch.GET,
-                headers={'Content-Type': 'application/json'}
-            )
-            response_json = response.content.decode('utf-8')
-        except urlfetch.Error as e:
-            print (e)
+        response: dict = urlfetch.fetch(
+            url='https://fhnw-ds-hs-2022-software-engineering-group2-ao7fiu5bra-oa.a.run.app/HousePricePrediction',
+            params=params,
+            method=urlfetch.GET,
+            headers={'Content-Type': 'application/json'}
+        )
+        response_json = response.content.decode('utf-8')
         #response_json: str = json.dumps(response)
         #response_json = json.dumps({"predicted_price":790000})
         db = get_db()
