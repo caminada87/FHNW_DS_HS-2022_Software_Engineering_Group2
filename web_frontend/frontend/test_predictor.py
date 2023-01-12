bc29595053ba4b1733eb5d45e3b1669978cfe2f9
Adrian <adrian.hausmann@bluewin.ch>
2023-01-12 13:43:19 +0100

    works until line 36

--- web_frontend/frontend/test_predictor.py
+++ web_frontend/frontend/test_predictor.py
@@ -2,49 +2,37 @@ import pytest
 import predictor
 from predictor import HousePricePrediction
 
+# fixture sind vorab funktion für pytest
+
 
 @pytest.fixture
-# fixture sind vorab funktion für pytest
 def get_bucket():
-    # Objekt zurückliefern
     return "bucket start"
 
 
+
+# parametrize stellt die testcases für pytest bereit
+
 @pytest.mark.parametrize(
-    # parametrize stellt die testcases für pytest bereit
-    "input_longitude", 
-    "input_latitude", 
-    "input_postal_code", 
-    "input_city", 
-    "input_bulding_category", 
-    "input_build_year", 
-    "input_living_area", 
-    "input_num_rooms", 
-    "result", 
-    "error", 
+    "input_longitude, input_latitude, input_postal_code, input_city, input_bulding_category, input_build_year, input_living_area, input_num_rooms, result, error", 
     [
-        (0, 0, 5420, 
-        "Ehrendingen", 
-        "Einfamilienhaus", 
-        2000, 200.0, 7, 
-        1750000.0, None)
+        (0, 0, 5420, "Ehrendingen", "Einfamilienhaus", 2000, 200.0, 7, 1750000.0, None) # Default Case
     ]
 )
 
 def test_HousePricePrediction(
     input_longitude: float, 
-    input_latitude: float,
-    input_postal_code: int,
-    input_city: str,
-    input_bulding_category: str,
-    input_build_year: int,
-    input_living_area: float,
-    input_num_rooms: float,
+    input_latitude: float, 
+    input_postal_code: int, 
+    input_city: str, 
+    input_bulding_category: str, 
+    input_build_year: int, 
+    input_living_area: float, 
+    input_num_rooms: float, 
     result: int, 
     get_bucket: str, 
     error: str
-) -> None:
-
+    ) -> None:
     assert "bucket start" == get_bucket()
     try:
         assert result == predictor.HousePricePrediction(
@@ -60,4 +48,4 @@ def test_HousePricePrediction(
     except:
         assert error != None
 
-# in bash-konsole mit Befehl "pytest" ausführen
+# in bash-konsole mit Befehl "pytest" oder "python -m pytest" ausführen
