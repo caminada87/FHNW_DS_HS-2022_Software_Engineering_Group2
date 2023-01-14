7000f2615cb97e2f475220a981214d0bbedf60b1
Stefan Caminada <caminada.stefan@gmail.com>
2023-01-14 09:51:30 +0100

    after merge

--- web_frontend/frontend/prediction.py
+++ web_frontend/frontend/prediction.py
@@ -14,14 +14,8 @@ import urlfetch
 
 bp = Blueprint('prediction', __name__)
 
-<<<<<<< HEAD
-@login_required
-@bp.route('/', methods=['GET', 'POST'])
-def index() -> str:
-=======
 @bp.route('/', methods=('GET', 'POST'))
 def index()->str:
->>>>>>> 00efb0d8d3a4ecc810fd0d4280051c1483897a0d
     if request.method == 'POST':
         longitude: float = request.form['longitude']
         latitude: float = request.form['latitude']
@@ -76,32 +70,3 @@ def index()->str:
             return render_template('prediction/index.html')
     else:
         return render_template('prediction/index.html')
-
-@bp.route('/recent')
-def recent()->str:
-    db = get_db()
-    #list of tuples
-    recent_predictions = db.execute(
-        'SELECT id, user_ip, query_data, predicted_price FROM predictions ORDER BY id DESC LIMIT 50'
-    ).fetchall()
-    predictions: list = []
-    for prediction in recent_predictions:
-        query_data = json.loads(prediction['query_data'])
-        prediction_data = json.loads(prediction['predicted_price'])
-        #import locale
-        #Doesn't work in Docker container:
-        #locale.setlocale(locale.LC_ALL, 'en_US.utf8')
-        predictions.append({'id': str(prediction['id']), 
-                            'user_ip': prediction['user_ip'],
-                            'longitude': query_data['longitude'],
-                            'latitude': query_data['latitude'], 
-                            'postal_code': query_data['postal_code'], 
-                            'city': query_data['city'],
-                            'building_category': query_data['bulding_category'],
-                            'build_year': query_data['build_year'],
-                            'living_area': query_data['living_area'],
-                            'num_rooms': query_data['num_rooms'],
-                            #'prediction': f"{int(prediction_data['predicted_price']):n}.- CHF".replace(',', '\'')
-                            'prediction': prediction_data['predicted_price']
-                          })
-    return render_template('prediction/recent.html', recent_predictions=predictions)
