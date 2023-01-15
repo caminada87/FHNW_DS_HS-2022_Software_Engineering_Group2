24e1c29db20c787626ce31663f27946f4c58a4ab
romansonder <roman.sonder@gmail.com>
2023-01-15 19:24:43 +0100

    Clean Code

--- web_frontend/tests/conftest.py
+++ web_frontend/tests/conftest.py
@@ -5,22 +5,24 @@ import pytest
 from frontend import create_app
 from frontend.db import get_db, init_db
 
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
+with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
+    _data_sql = f.read().decode("utf8")
 
 
 @pytest.fixture
 def app() -> None:
     """
-
+    Create test web app.
     """
     db_fd, db_path = tempfile.mkstemp()
 
-    app = create_app({
-        'TESTING': True,
-        'BASE_URL': 'http://localhost:5000',
-        'DATABASE': db_path,
-    })
+    app = create_app(
+        {
+            "TESTING": True,
+            "BASE_URL": "http://localhost:5000",
+            "DATABASE": db_path,
+        }
+    )
 
     with app.app_context():
         init_db()
@@ -33,31 +35,49 @@ def app() -> None:
 
 
 @pytest.fixture
-def client(app):
+def client(app) -> None:
+    """
+    Run test_client on app.
+    """
     return app.test_client()
 
 
 @pytest.fixture
 def runner(app):
+    """
+    Test runner.
+    """
     return app.test_cli_runner()
 
 
 class AuthActions(object):
-    """The user has to be logged in most of the times. Also during testing..."""
+    """
+    The user has to be logged in most of the time. Also, during testing.
+    """
 
     def __init__(self, client):
         self._client = client
 
-    def login(self, email='test@tester.com', password='1234'):
+    def login(self, email="test@tester.com", password="1234") -> None:
+        """
+        Login with test user.
+        @param email: test user e-mail
+        @param password: test user password
+        """
         return self._client.post(
-            '/auth/login',
-            data={'email': email, 'password': password}
+            "/auth/login", data={"email": email, "password": password}
         )
 
-    def logout(self):
-        return self._client.get('/auth/logout')
+    def logout(self) -> None:
+        """
+        User logout.
+        """
+        return self._client.get("/auth/logout")
 
 
 @pytest.fixture
-def auth(client):
+def auth(client) -> AuthActions:
+    """
+    Test authentication.
+    """
     return AuthActions(client)
--- web_frontend/tests/test_prediction.py
+++ web_frontend/tests/test_prediction.py
@@ -1,15 +1,14 @@
-import pytest
-from frontend.db import get_db
-
-def test_index(client, auth):
-    """Tests redirection if not logged and Home page display if logged in."""
-    response = client.get('/')
+def test_index(client, auth) -> None:
+    """
+    Tests redirection if not logged and Home page display if logged in.
+    """
+    response = client.get("/")
     assert response.headers["Location"] == "/auth/login"
 
     auth.login()
-    response = client.get('/')
-    assert b'Logout' in response.data
-    assert b'Hauspreise' in response.data
-    assert b'Postleitzahl' in response.data
-    assert b'Anzahl Zimmer' in response.data
+    response = client.get("/")
+    assert b"Logout" in response.data
+    assert b"Hauspreise" in response.data
+    assert b"Postleitzahl" in response.data
+    assert b"Anzahl Zimmer" in response.data
     assert b'form method="post"' in response.data
